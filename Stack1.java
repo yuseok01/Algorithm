a78da9550f5162fe22329b3c012df387946ff616
itgorae <itgorae.seok@gmail.com>
2024-12-19 14:54:51 +0900

    1219

--- Stack1.java
+++ Stack1.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class Stack1 {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>() ;
         for(int i = 0 ; i < s.length(); i++){
             if(s.charAt(i)=='('){
                 stack.add('(');
